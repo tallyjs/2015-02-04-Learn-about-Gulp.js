e3ec73db00a2929f12da9fa650e4c7d2f414b894
analog-nico <nicolai.kamenzky@testrails.org>
2015-02-02 23:50:23 -0500

    Added JSHint

--- demo/gulpfile.js
+++ demo/gulpfile.js
@@ -1,6 +1,5 @@
 'use strict';
 // autoprefix
-// jshint
 // imagemin
 
 var gulp = require('gulp');
@@ -9,6 +8,9 @@ var uglify = require('gulp-uglify');
 var rename = require("gulp-rename");
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
+var filter = require('gulp-filter');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
 
 
 gulp.task('dev', [
@@ -38,13 +40,20 @@ gulp.task('watch-js', ['build-js'], function () {
 
 gulp.task('build-js', function () {
 
-    gulp.src([
+    var filterAppJs = filter('./js/app.js');
+
+    return gulp.src([
             'bower_components/todomvc-common/base.js',
             'bower_components/jquery/dist/jquery.js',
             'bower_components/handlebars/handlebars.js',
             'bower_components/director/build/director.js',
             './js/app.js'
         ])
+        .pipe(filterAppJs)
+            .pipe(jshint())
+            .pipe(jshint.reporter('jshint-stylish'))
+            .pipe(jshint.reporter('fail'))
+        .pipe(filterAppJs.restore())
         .pipe(sourcemaps.init())
         .pipe(concat('app.js'))
         .pipe(uglify())
