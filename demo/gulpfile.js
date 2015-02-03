53d4466acdb05e5e1a864268a3c9e8d971d18f2c
analog-nico <nicolai.kamenzky@testrails.org>
2015-02-02 22:47:49 -0500

    Added JS minifaction

--- demo/gulpfile.js
+++ demo/gulpfile.js
@@ -8,10 +8,13 @@
 
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
+var uglify = require('gulp-uglify');
+var rename = require("gulp-rename");
 
 
 gulp.task('dev', [
-    'browser-sync'
+    'browser-sync',
+    'watch-js'
 ]);
 
 gulp.task('browser-sync', function () {
@@ -27,5 +30,20 @@ gulp.task('browser-sync', function () {
         '!./bower_components/**/*',
         '!./node_modules/**/*'
     ], browserSync.reload);
+
+});
+
+gulp.task('watch-js', ['build-js'], function () {
+    gulp.watch('./js/app.js', ['build-js']);
+});
+
+gulp.task('build-js', function () {
+
+    gulp.src('./js/app.js')
+        .pipe(uglify())
+        .pipe(rename(function (path) {
+            path.extname = '.min.js';
+        }))
+        .pipe(gulp.dest('build'));
 
 });
